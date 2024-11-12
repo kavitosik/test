9784d54fa59f0aca01cd65afcaa1aa46e733325d
Kirill <km220410@gmail.com>
2024-11-12 13:54:25 +0000

    прошёл тестирование

--- main.py
+++ main.py
@@ -6,12 +6,24 @@
 # num = int(input('Введите число: '))
 # print(is_even(num))
 
-my_list1 = [1, 2, 5, 7, 'привет']
-my_list2 = [1,'привет', 6, 5, 21]
-def common_elements(list1, list2):
-    my_list = []
-    for i in list1:
-        if i in list2:
-            my_list.append(i)
-    return my_list
-print(common_elements(my_list1, my_list2))
+# my_list1 = [1, 2, 5, 7, 'привет']
+# my_list2 = [1,'привет', 6, 5, 21]
+# def common_elements(list1, list2):
+#     my_list = []
+#     for i in list1:
+#         if i in list2:
+#             my_list.append(i)
+#     return my_list
+# print(common_elements(my_list1, my_list2))
+
+
+# def find_index(lst, value):
+#     for i in range(len(lst)):
+#         if value == lst[i]:
+#             return i
+
+# my_list = [1, 2, 3, 4, 5, 6]
+# x = 5
+# print(find_index(my_list, x))
+
+
